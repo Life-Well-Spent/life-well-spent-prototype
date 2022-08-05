e8222cbcb64bb069300ee803656a52e7f4afcd89
Xander Warszawski <xander@xdoubleu.com>
2022-08-05 16:24:08 +0200

    Update pages/urls.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 
 from .views import HelloWorldView
 
-urlpatterns = [
-    path('', HelloWorldView.as_view(), name="hello_world")
-]
+urlpatterns = [path("", HelloWorldView.as_view(), name="hello_world")]
