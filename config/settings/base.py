e348b3b729d5d5946da62127521595dcc3204cbf
Xander Warszawski <xander@xdoubleu.com>
2022-08-05 16:22:37 +0200

    Update config/settings/base.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from environs import Env
 from django.core.management.utils import get_random_secret_key
 
 env = Env()
