4a162beefc50ef07e6919a0632dde51f2599a525
Xander Warszawski <xander@xdoubleu.com>
2022-08-22 11:48:29 +0200

    Update config/settings/base.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import sentry_sdk
 from django.core.management.utils import get_random_secret_key
 from environs import Env
 
