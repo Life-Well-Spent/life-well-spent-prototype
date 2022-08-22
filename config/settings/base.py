a064721d0a8326414d6695dd18b45a82a09a300d
Xander Warszawski <xander@xdoubleu.com>
2022-08-22 11:49:10 +0200

    Update config/settings/base.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -218,5 +218,5 @@ if len(SENTRY_DSN) > 0:
         traces_sample_rate=1.0,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
+        send_default_pii=True,
     )
