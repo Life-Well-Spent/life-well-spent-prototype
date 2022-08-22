f4b40c9b851f658e4d3f4a2f8002dfd7b353290d
Xander Warszawski <xander@xdoubleu.com>
2022-08-22 12:08:24 +0200

    Added env variable for closing signup

---
+++ config/adapter.py
@@ -0,0 +1,21 @@
+from allauth.account.adapter import DefaultAccountAdapter
+
+from environs import Env
+
+class NoNewUsersAccountAdapter(DefaultAccountAdapter):
+    """ 
+    Adapter to disable allauth new signups
+    Used at equilang/settings.py with key ACCOUNT_ADAPTER
+
+    https://django-allauth.readthedocs.io/en/latest/advanced.html#custom-redirects """
+    
+    def is_open_for_signup(self, request):
+        env = Env()
+        env.read_env()
+        """
+        Checks whether or not the site is open for signups.
+
+        Next to simply returning True/False you can also intervene the
+        regular flow by raising an ImmediateHttpResponse
+        """
+        return env.bool("DJANGO_SIGNUP_OPEN", True)
